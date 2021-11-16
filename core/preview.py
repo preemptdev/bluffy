8b32a419e5798c6e30573c8d7d463ba514a01df2
Michael Ranaldo <michael.ranaldo@outlook.com>
2021-11-16 22:20:21 +0000

    Added nicer theme

--- core/preview.py
+++ core/preview.py
@@ -4,5 +4,5 @@ from rich.syntax import Syntax
 def print_preview(code, language):
 	print()
 	console = Console()
-	console.print(Syntax(code, language))
+	console.print(Syntax(code, language, line_numbers=True, theme="darcula"))
 	print()
