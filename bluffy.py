6685d5dace4e78e229a142c1bd57da4cc47d6446
mez0 <mez0@pm.me>
2021-11-18 15:05:18 +0000

    reduced args

--- bluffy.py
+++ bluffy.py
@@ -4,28 +4,28 @@ from core.mask.MaskFactory import get_mask_factory, get_factories, MaskFactory
 from core import logger, banner, preview
 
 
-def mask_bin(input_file: str, mask_required: str, preview_file: bool) -> None:
+def mask_bin(args) -> bool:
     """Given an input, output, and mask type: read the bytes, identify the factory, mask the bytes, write them to disk."""
 
-    if input_file == None or mask_required == None:
+    if args.bin == None or args.mask == None:
         logger.bad("Please specify -b AND -m (bin file and mask)")
-        return 
+        return False
 
     # get the bytes of the input bin
-    blob: bytes = helpers.get_bytes_from_file(input_file)
+    blob: bytes = helpers.get_bytes_from_file(args.bin)
 
     # if that isnt possible, return.
     if blob == None:
-        return
+        return False
 
-    logger.info(f"Loaded {input_file} ({len(blob)} bytes)")
+    logger.info(f"Loaded {args.bin} ({len(blob)} bytes)")
 
     # get the correct factory
-    factory = get_mask_factory(mask_required)
+    factory = get_mask_factory(args.mask)
 
     # if that fails, return.
     if factory == None:
-        return
+        return False
 
     # if the factory is obtained, grab the class for the mask
     mask = factory.get_mask_type()
@@ -34,7 +34,7 @@ def mask_bin(input_file: str, mask_required: str, preview_file: bool) -> None:
     # give the blob to the class and perform whatever transformations... This should then return a multiline string containing the transformed data
     bluff = mask.mask(blob)
 
-    if (preview_file):
+    if args.preview:
         logger.info(f"Loading preview:")
         preview.print_preview(bluff, "c")
 
@@ -43,7 +43,7 @@ def mask_bin(input_file: str, mask_required: str, preview_file: bool) -> None:
     helpers.write_text_to_cfile(factory.name, bluff)
 
     # hopefully its done!
-    return
+    return True
 
 
 def show_masks() -> None:
@@ -66,8 +66,7 @@ def main() -> None:
         quit()
 
     # otherwise do the thing!
-    else:
-        mask_bin(input_file=args.bin, mask_required=args.mask, preview_file=args.preview)
+    mask_bin(args)
 
     return
 
